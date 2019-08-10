a01dfb55194d77259be7d0ab8ad40015ea1fbf5f
Peter Tandler <p.github@tandlers.de>
2019-08-11 00:04:05 +0200

    Added dist version in `docs`

---
+++ docs/precache-manifest.221b74ac971da100f06bd2d66588884c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "900d092e54a863063911",
+    "url": "js/chunk-vendors.45dd49a3.js"
+  },
+  {
+    "revision": "66680bee0309fe595173",
+    "url": "js/app.fcbbab0c.js"
+  },
+  {
+    "revision": "131c2443354cbf18ae82aabaeff826f7",
+    "url": "index.html"
+  },
+  {
+    "revision": "66680bee0309fe595173",
+    "url": "css/app.b6c6453a.css"
+  }
+];
---
+++ docs/precache-manifest.6fd982f8eb82b5c31df519f82b886a81.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f08fd9b4f19723a0f341",
+    "url": "js/chunk-vendors-legacy.fa48e6bc.js"
+  },
+  {
+    "revision": "eadd2787822cbbe2bf68",
+    "url": "js/app-legacy.57ab09c1.js"
+  },
+  {
+    "revision": "c438627a8bddd57cd451d240f383bdc2",
+    "url": "index.html"
+  },
+  {
+    "revision": "eadd2787822cbbe2bf68",
+    "url": "css/app.b6c6453a.css"
+  }
+];
